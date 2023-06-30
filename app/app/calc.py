a0a185e362eb55cc06027cc914102b70f260b418
Juan Hernandez <juanguille6070@hotmail.com>
2023-06-30 09:32:57 -0500

    added DB

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator function
+"""
+
+def add(x, y):
+    """add x and y and return result"""
+    return x + y
+
+
+def substract(x, y):
+    """Substract x from y """
+
+    return y -x
